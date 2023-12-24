79a6d1ea0e3e233fc10116d390bd93332924ba2c
sr-ndai <srogachev@neuraldynamicsai.com>
2023-12-24 12:59:32 -0500

    fix: wrappers

--- qint/qint.py
+++ qint/qint.py
@@ -1,69 +1,64 @@
 from decimal import Decimal
 from fractions import Fraction
-from functools import total_ordering, wraps
-from typing import NamedTuple, Optional, Self
+from functools import wraps
+from typing import Callable, NamedTuple, Optional, Self, Tuple, TypeVar
 
 import qint.utils as ut
 
 from .exceptions import QIntPrecisionError, QIntTypeError
 from .utils import Number
 
+T = TypeVar("T", bound="QInt")
+Scalar = int | Fraction
+Method = Callable[[T, T | Scalar], T]
 
-def check_operand(valid_types: tuple[type, ...], operation: str):
+
+def check_operand(valid_types: Optional[Tuple[type, ...]] = None) -> Method:
     """
     Decorator for checking the type of the operand in QInt operations. The
-    operand must be of one of the types in `valid_types`.
+    operand must be of one of the types in `valid_types`. If `valid_types` is
+    not provided, only QInt is considered valid.
+
+    :param valid_types: A tuple of types that are valid for the operation, or None to
+        default to QInt only.
+    :return: A callable that takes a QInt instance and an operand.
     """
 
-    def decorator(method):
+    if valid_types is None:
+        valid_types = tuple()
+
+    def decorator(method: Method) -> Method:
         @wraps(method)
-        def wrapper(self: QInt, other):
-            if not isinstance(other, (QInt, *valid_types)):
+        def wrapper(self: T, other: T | Scalar, *args, **kwargs) -> T:
+            if not isinstance(other, (type(self), *valid_types)):
+                operation = method.__name__
                 raise QIntTypeError(other, operation)
-
-            if isinstance(other, QInt) and self.precision != other.precision:
-                raise QIntPrecisionError(self.precision, other.precision)
-
-            return method(self, other)
+            return method(self, other, *args, **kwargs)
 
         return wrapper
 
     return decorator
 
 
-def scale_safe(operation: str):
+def require_same_precision(method: Method) -> Method:
     """
-    Scale a QInt to a given precision. This is useful when we want to perform
-    operations on QInts with different precisions. We scale both QInts to the
-    same precision, perform the operation, and then scale the result back to the
-    original precision.
+    Decorator for arithmetic methods of QInt to ensure that the precision of
+    both operands match when the other operand is a QInt. Scalars like ints or
+    Fractions are treated as such and do not require a precision check.
+
+    :param method: The arithmetic method to be wrapped.
+    :return: Wrapped method that checks precision compatibility.
     """
 
-    def decorator(method):
-        @wraps(method)
-        def wrapper(self: QInt, other):
-            if not isinstance(other, QInt):
-                raise QIntTypeError(other, operation)
+    @wraps(method)
+    def wrapper(self: T, other: T | Scalar) -> T:
+        if isinstance(other, QInt) and self.precision != other.precision:
+            raise QIntPrecisionError(self.precision, other.precision)
+        return method(self, other)
 
-            max_precision = max(self.precision, other.precision)
-
-            def scale(q: QInt) -> QInt:
-                precision_difference = max_precision - q.precision
-
-                if precision_difference == 0:
-                    return q  # Precision stays the same
-
-                scaled_value = ut.scale(q.value, precision_difference)
-                return QInt(scaled_value, max_precision)
-
-            return method(scale(self), scale(other))
-
-        return wrapper
-
-    return decorator
+    return wrapper
 
 
-@total_ordering
 class QInt(NamedTuple):
     """
     Class for representing quantized integers. The value is stored as an
@@ -125,25 +120,32 @@ class QInt(NamedTuple):
     def __int__(self) -> int:
         return int(round(self.__float__()))
 
+    @check_operand((int,))
+    @require_same_precision
     def __add__(self, other: Self | int) -> Self:
         if isinstance(other, QInt):
             return QInt(self.value + other.value, self.precision)
         else:
             return QInt(self.value + other, self.precision)
 
+    @check_operand()
     def add(self, other: Self, targ: Optional[int] = None) -> Self:
         return self.__add__(other).scale(targ)
 
+    @check_operand((int,))
+    @require_same_precision
     def __sub__(self, other: Self | int) -> Self:
         if isinstance(other, QInt):
             return QInt(self.value - other.value, self.precision)
         else:
             return QInt(self.value - other, self.precision)
 
+    @check_operand()
     def sub(self, other: Self, targ: Optional[int] = None) -> Self:
         return self.__sub__(other).scale(targ)
 
-    def __mul__(self, other: Self | int | Fraction) -> Self:
+    @check_operand((Scalar,))
+    def __mul__(self, other: Self | Scalar) -> Self:
         if isinstance(other, QInt):
             return QInt(self.value * other.value, self.precision + other.precision)
         elif isinstance(other, Fraction):
@@ -152,10 +154,12 @@ class QInt(NamedTuple):
         else:
             return QInt(self.value * other, self.precision)
 
+    @check_operand()
     def mul(self, other: Self, targ: int) -> Self:
         return self.__mul__(other).scale(targ)
 
-    def __truediv__(self, other: Self | int | Fraction) -> Self:
+    @check_operand((Scalar,))
+    def __truediv__(self, other: Self | Scalar) -> Self:
         if isinstance(other, QInt):
             value = ut.banker_division(self.value, other.value)
             return QInt(value, self.precision - other.precision)
@@ -166,12 +170,14 @@ class QInt(NamedTuple):
             value = ut.banker_division(self.value, other)
             return QInt(value, self.precision)
 
+    @check_operand()
     def div(self, other: Self, targ: Optional[int] = None) -> Self:
         # for div we scale upfront to avoid precision loss
         div_prec = other.precision + targ if targ is not None else None
         return self.scale(div_prec).__truediv__(other)
 
-    def __floordiv__(self, other: Self | int | Fraction) -> Self:
+    @check_operand((Scalar,))
+    def __floordiv__(self, other: Self | Scalar) -> Self:
         if isinstance(other, QInt):
             value = self.value // other.value
             return QInt(value, self.precision - other.precision)
@@ -182,15 +188,11 @@ class QInt(NamedTuple):
             value = self.value // other
             return QInt(value, self.precision)
 
+    @check_operand()
+    @require_same_precision
     def __mod__(self, other: Self | int) -> Self:
-        if isinstance(other, QInt):
-            if self.precision != other.precision:
-                raise ValueError(
-                    "Modulo operation requires same precision for both QInts"
-                )
-
-            mod_value = self.value % other.value
-            return QInt(mod_value, self.precision)
+        mod_value = self.value % other.value
+        return QInt(mod_value, self.precision)
 
     def __pow__(self, other: int) -> Self:
         """
@@ -244,20 +246,32 @@ class QInt(NamedTuple):
     def __abs__(self) -> Self:
         return QInt(abs(self.value), self.precision)
 
+    @check_operand()
+    @require_same_precision
     def __eq__(self, __obj: Self) -> bool:
         return self.value == __obj.value
 
+    @check_operand()
+    @require_same_precision
     def __ne__(self, __obj: Self) -> bool:
         return self.value != __obj.value
 
+    @check_operand()
+    @require_same_precision
     def __gt__(self, __obj: Self) -> bool:
         return self.value > __obj.value
 
+    @check_operand()
+    @require_same_precision
     def __ge__(self, __obj: Self) -> bool:
         return self.value >= __obj.value
 
+    @check_operand()
+    @require_same_precision
     def __lt__(self, __obj: Self) -> bool:
         return self.value < __obj.value
 
+    @check_operand()
+    @require_same_precision
     def __le__(self, __obj: Self) -> bool:
         return self.value <= __obj.value
